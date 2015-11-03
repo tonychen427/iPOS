888dab5452c63f56d025839b2c64e37fcda72ddb
Tony Chen <tchen427@gmail.com>
2015-11-02 23:47:38 -0800

    update

---
+++ com.clientsbox.ipos.logic/src/main/java/com/clientsbox/ipos/logic/DownloadManager.java
@@ -0,0 +1,39 @@
+package com.clientsbox.ipos.logic;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Insprion on 11/2/15.
+ */
+public class DownloadManager {
+
+    private static void downloadFile(String url, File outputFile) {
+        try {
+            URL u = new URL(url);
+            URLConnection conn = u.openConnection();
+            int contentLength = conn.getContentLength();
+
+            DataInputStream stream = new DataInputStream(u.openStream());
+
+            byte[] buffer = new byte[contentLength];
+            stream.readFully(buffer);
+            stream.close();
+
+            DataOutputStream fos = new DataOutputStream(new FileOutputStream(outputFile));
+            fos.write(buffer);
+            fos.flush();
+            fos.close();
+        } catch(FileNotFoundException e) {
+            return; // swallow a 404
+        } catch (IOException e) {
+            return; // swallow a 404
+        }
+    }
+}
